793cac25d9e82a3d0e48f7f2f4c40fb432e276c7
Trevor Cooper <tcooper@sdsc.edu>
2019-12-20 11:32:19 -0800

    Copyright Storm version 7.0

--- src/rocks-command-imgstorage/rocks/commands/report/host/imgstorage/__init__.py
+++ src/rocks-command-imgstorage/rocks/commands/report/host/imgstorage/__init__.py
@@ -1,44 +1,46 @@
+#!/opt/rocks/bin/python
 #
 # @Copyright@
-# 
-# 				Rocks(r)
-# 		         www.rocksclusters.org
-# 		         version 6.2 (SideWinder)
-# 
-# Copyright (c) 2000 - 2014 The Regents of the University of California.
-# All rights reserved.	
-# 
+#
+#                                 Rocks(r)
+#                          www.rocksclusters.org
+#                          version 6.2 (SideWinder)
+#                          version 7.0 (Manzanita)
+#
+# Copyright (c) 2000 - 2017 The Regents of the University of California.
+# All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright
 # notice unmodified and in its entirety, this list of conditions and the
-# following disclaimer in the documentation and/or other materials provided 
+# following disclaimer in the documentation and/or other materials provided
 # with the distribution.
-# 
+#
 # 3. All advertising and press materials, printed or electronic, mentioning
-# features or use of this software must display the following acknowledgement: 
-# 
-# 	"This product includes software developed by the Rocks(r)
-# 	Cluster Group at the San Diego Supercomputer Center at the
-# 	University of California, San Diego and its contributors."
-# 
+# features or use of this software must display the following acknowledgement:
+#
+#         "This product includes software developed by the Rocks(r)
+#         Cluster Group at the San Diego Supercomputer Center at the
+#         University of California, San Diego and its contributors."
+#
 # 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
 # neither the name or logo of this software nor the names of its
 # authors may be used to endorse or promote products derived from this
 # software without specific prior written permission.  The name of the
 # software includes the following terms, and any derivatives thereof:
-# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
-# the associated name, interested parties should contact Technology 
-# Transfer & Intellectual Property Services, University of California, 
-# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of
+# the associated name, interested parties should contact Technology
+# Transfer & Intellectual Property Services, University of California,
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910,
 # Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
-# 
-# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
 # PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
@@ -49,8 +51,9 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # @Copyright@
+#
 #
 
 import sys
@@ -60,7 +63,7 @@ import string
 class Command(rocks.commands.HostArgumentProcessor,
 	rocks.commands.report.command):
 	"""
-	create /etc/imgstorage.conf	
+	create /etc/imgstorage.conf
 
 	<arg optional='0' type='string' name='host'>
 	Host name of machine
@@ -73,14 +76,14 @@ class Command(rocks.commands.HostArgumentProcessor,
 
 	<param optional='1' type='string' name='pool'>
 	Define the name of the pool to use for local volumes.
-	Overrides host attribute imgstorage_pool 
+	Overrides host attribute imgstorage_pool
 	</param>
 
 	<param optional='1' type='string' name='workers'>
 	set the number of synchronization workers overriding both the default (8) and
 	host attribute imgstorage_workers (if it exists)
 	</param>
-	
+
 	<example cmd='report host imgstorage vm-container-0-0'>
 	Create the image storage configuration file for vm-container-0-0.
 	</example>
@@ -107,23 +110,23 @@ class Command(rocks.commands.HostArgumentProcessor,
 
 		self.beginOutput()
 		for host in self.getHostnames(args):
-			private = self.command('list.network', 
+			private = self.command('list.network',
 				['private','output-col=dnszone', 'output-header=no']).strip()
 			if network is None:
 				network = self.db.getHostAttr(host,'imgstorage-network')
 			network = private if network is None else network
 
-		
+
 			if default_pool is None:
 				default_pool = self.db.getHostAttr(host,'imgstorage-pool')
 			old_pool = self.db.getHostAttr(host,'vm_container_zpool')
 			default_pool = old_pool if default_pool is None else default_pool
 
-			
+
 			if sync_workers is None:
 				sync_workers = self.db.getHostAttr(host,'imgstorage-workers')
 			sync_workers = DEFAULT_WORKERS if sync_workers is None else sync_workers
-			
+
 			self.addOutput(host, '<file name="/etc/imgstorage.conf">')
 			self.addOutput(host, '[ {')
 			self.addOutput(host, '"name" : "%s",' % host)
--- src/rocks-command-imgstorage/rocks/commands/set/host/zvolattr/__init__.py
+++ src/rocks-command-imgstorage/rocks/commands/set/host/zvolattr/__init__.py
@@ -1,5 +1,59 @@
 #!/opt/rocks/bin/python
 #
+# @Copyright@
+#
+#                                 Rocks(r)
+#                          www.rocksclusters.org
+#                          version 6.2 (SideWinder)
+#                          version 7.0 (Manzanita)
+#
+# Copyright (c) 2000 - 2017 The Regents of the University of California.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice unmodified and in its entirety, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. All advertising and press materials, printed or electronic, mentioning
+# features or use of this software must display the following acknowledgement:
+#
+#         "This product includes software developed by the Rocks(r)
+#         Cluster Group at the San Diego Supercomputer Center at the
+#         University of California, San Diego and its contributors."
+#
+# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
+# neither the name or logo of this software nor the names of its
+# authors may be used to endorse or promote products derived from this
+# software without specific prior written permission.  The name of the
+# software includes the following terms, and any derivatives thereof:
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of
+# the associated name, interested parties should contact Technology
+# Transfer & Intellectual Property Services, University of California,
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910,
+# Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# @Copyright@
+#
 #
 
 import sys
@@ -20,30 +74,30 @@ import datetime
 class Command(rocks.commands.HostArgumentProcessor, rocks.commands.set.command):
     """
    Set a global attribute for the daemon running on a  NAS (or virtual machine images repository).
-    
+
     <arg type='string' name='nas' optional='0'>
-    NAS on  which we want to set an attribute 
+    NAS on  which we want to set an attribute
     </arg>
 
     <arg type='string' name='zvol' optional='0'>
-    zvol for which the the attribute should be set 
+    zvol for which the the attribute should be set
     </arg>
 
     <arg type='string' name='attr' optional='0'>
-    Attribute to set 
+    Attribute to set
     </arg>
 
     <arg type='string' name='value' optional='0'>
     Value to set named attribute.  'None' will set it to a python
-    None value 
+    None value
     </arg>
 
     <example cmd='set host zvolattr nas-0-0 hosted-vm-0-0-0-vol frequency 900'>
-    Set the frequency of synchronization for zvol hosted-vm-0-0-0-vol to 900 seconds 
+    Set the frequency of synchronization for zvol hosted-vm-0-0-0-vol to 900 seconds
     </example>
 
     <example cmd='set host zvolattr nas-0-0 hosted-vm-0-0-0-vol frequency None'>
-    set frequency to the default system attribute 
+    set frequency to the default system attribute
     </example>
     """
 
